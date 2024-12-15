02d3683b307636e7cf80489727bc43cc3f279d02
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-15 07:03:48 +0700

    Menambahkan rute navigasi untuk halaman home, detail, dan update.

--- app/src/main/java/com/example/roomlocaldb/ui/theme/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/theme/navigation/AlamatNavigasi.kt
@@ -3,3 +3,16 @@ package com.example.roomlocaldb.ui.theme.navigation
 interface AlamatNavigasi {
     val route: String
 }
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val NIM = "nim"
+    val routerWithArg = "$route/{$NIM}"
+}
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val NIM = "nim"
+    val routeWithArg = "$route/${NIM}"
+}
