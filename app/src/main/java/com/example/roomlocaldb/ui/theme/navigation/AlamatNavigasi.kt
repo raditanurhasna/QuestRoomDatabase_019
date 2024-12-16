ee85f3dcec13eeadf5ac4de9e3819d9735e7145f
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-16 21:49:27 +0700

    memperbaiki codingan yanag error.

--- app/src/main/java/com/example/roomlocaldb/ui/theme/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/theme/navigation/AlamatNavigasi.kt
@@ -3,16 +3,19 @@ package com.example.roomlocaldb.ui.theme.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
 object DestinasiHome : AlamatNavigasi {
     override val route = "home"
 }
+
 object DestinasiDetail : AlamatNavigasi {
     override val route = "detail"
     const val NIM = "nim"
-    val routerWithArg = "$route/{$NIM}"
+    val routesWithArg = "$route/{$NIM}"
 }
+
 object DestinasiUpdate : AlamatNavigasi {
     override val route = "update"
     const val NIM = "nim"
-    val routeWithArg = "$route/${NIM}"
+    val routesWithArg = "$route/{$NIM}"
 }
