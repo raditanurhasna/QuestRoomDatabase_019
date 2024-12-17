f1d40aff904942926d2dbc7901c9adf8e858540d
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-17 09:06:33 +0700

    Memperbaiki codingan yang sempaterror.

--- app/src/main/java/com/example/roomlocaldb/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb/repository/LocalRepositoryMhs.kt
@@ -1,13 +1,30 @@
 package com.example.roomlocaldb.repository
 
-import com.example.roomlocaldb.data.DAO.MahasiswaDAO
+import com.example.roomlocaldb.data.DAO.MahasiswaDao
 import com.example.roomlocaldb.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
-class LocalRepositoryMhs (
-    private val  mahasiswaDao : MahasiswaDAO
-    ): RepositoryMhs {
+class LocalRepositoryMhs(
+    private val mahasiswaDao: MahasiswaDao
+) : RepositoryMhs {
+
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
 
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
 }
